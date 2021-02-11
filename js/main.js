3d4337c2b15792d970b012095f752d28f509a0d5
Alexander Tikhonov <at1385@yandex.ru>
2021-02-11 19:39:45 +0500

    Создаёт константы для генерации временных данных

--- js/main.js
+++ js/main.js
@@ -25,3 +25,19 @@ const getRandomFloatInclusive = (min, max, precision) => {
 }
 
 getRandomFloatInclusive(1.1, 1.2, 5);
+
+// module3-task1
+const AVATARS = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const APARTMENT_TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const APARTMENT_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const APARTMENT_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
+
+const X_MIN = 35.65000;
+const X_MAX = 35.70000;
+const Y_MIN = 139.70000;
+const Y_MAX = 139.80000;
+
+const MAX_COUNT = 10;
