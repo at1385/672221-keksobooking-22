a231cd61b1fc265b0037b17e8439579cd14c64c0
Alexander Tikhonov <at1385@yandex.ru>
2021-03-13 22:02:50 +0500

    [main] Редактирует подключение модулей в точке входа

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import './create-ads.js';
-import './render-ad-card.js';
-import './form.js';
 import './deactivator.js';
 import './activator.js';
+import './create-ads.js';
+import './render-ad-card.js';
 import './map.js';
 import './filters.js';
+import './form.js';
