8d3000ff099fe2f50d3d22710f23aabe5a2d94e2
Alexander Tikhonov <at1385@yandex.ru>
2021-03-25 23:20:35 +0500

    [util] Создаёт функцию вывода сообщения о неудачном получении данных

--- js/util.js
+++ js/util.js
@@ -12,7 +12,22 @@ const hideNode = (node) => {
   node.style.display = 'none';
 }
 
+const showAlert = (message) => {
+  const alertBlock = document.createElement('div');
+  alertBlock.style.position = 'absolute';
+  alertBlock.style.top = 0;
+  alertBlock.style.right = 0;
+  alertBlock.style.left = 0;
+  alertBlock.style.zIndex = 100;
+  alertBlock.style.padding = '10px 3px';
+  alertBlock.style.fontSize = '30px';
+  alertBlock.style.color = 'white';
+  alertBlock.style.textAlign = 'center';
+  alertBlock.style.background = 'red';
 
+  alertBlock.textContent = message;
 
+  document.body.appendChild(alertBlock);
 };
 
+export {getCorrectEndingWord, hideNode, showAlert};
