999eb7e66f24c6db25adb98a279f056b1986a133
Alexander Tikhonov <at1385@yandex.ru>
2021-03-29 22:02:28 +0500

    [server] Создаёт функцию отправки данных

--- js/server.js
+++ js/server.js
@@ -1,5 +1,8 @@
-const getData = async () => {
-  const response = await fetch('https://22.javascript.pages.academy/keksobooking/data');
+const ADS_DATA_URL = 'https://22.javascript.pages.academy/keksobooking/data';
+const AD_FORM_URL = 'https://22.javascript.pages.academy/keksobooking';
+
+const getData = async (url) => {
+  const response = await fetch(url);
 
   if (response.ok) {
     const data = await response.json();
@@ -7,4 +10,24 @@ const getData = async () => {
   }
 };
 
-export {getData};
+const sendData = (body, url, onSuccess, onError) => {
+  fetch(
+    url,
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onError();
+      }
+    })
+    .catch(() => {
+      onError();
+    });
+}
+
+export {ADS_DATA_URL, AD_FORM_URL, getData, sendData};
